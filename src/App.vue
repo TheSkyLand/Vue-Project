11637396911e41f4d60ebc681e36e720f6247f88
George <77775699+TheSkyLand@users.noreply.github.com>
2025-02-15 20:23:37 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <RouterView />
+  <WebVue />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+import WebVue from './views/WebVue.vue';
+</script>
+
+
+
+<style scoped>
+
+</style>
