bbddf0a3836f9586efdb35847bf6e4d104643cbc
George <77775699+TheSkyLand@users.noreply.github.com>
2025-02-25 15:04:21 +0300

    Add files via upload

---
+++ YandexTask/src/views/JoskiiView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+
+
+
+</script>
+
+<style></style>
+
+<template>
+    <div>
+        <input v-mod type="number" min="1" max="1000" v-model="KString" placeholder="Enter K" />
+
+        <span>K:{{ KString }}</span>
+    </div>
+    <div>
+        
+        <input type="number" min="1" max="1000" v-model="NString" placeholder="Enter N" />
+
+        <span>N:{{ NString }}</span>
+    </div>
+    <button @click="console.log( KString ,  NString)">
+        active
+    </button>
+    <div>
+        winner: {{ WinnerString }}
+    </div>
+</template>
