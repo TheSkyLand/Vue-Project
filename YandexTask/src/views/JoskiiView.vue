6451855b0cd262ca5c4753f57810aa59d816e3aa
George <77775699+TheSkyLand@users.noreply.github.com>
2025-02-26 12:34:27 +0300

    Update JoskiiView.vue

--- YandexTask/src/views/JoskiiView.vue
+++ YandexTask/src/views/JoskiiView.vue
@@ -1,25 +1,37 @@
 <script setup>
+const K = 3
+const N = 1
 
-
-
-
+let SashaScore;
+let LenaScore;
+const play = () => {
+    if ((N % 5) == 0){
+        LenaScore++;
+    }
+    if ((N % 3) == 0) {
+        SashaScore++;
+    }
+    if (((N % 3) == 0) && ((N % 5) == 0) && ((N % 3) !== 0) && ((N % 5) !== 0)) 
+    {
+        LenaScore += 0;
+        SashaScore += 0;  
+    }
+    if (SashaScore > LenaScore) {
+        WinnerString = 'sasha'
+    }
+    else if (LenaScore > SashaScore) {
+        WinnerString = 'lena'
+    }
+    else {
+        WinnerString = 'draw'
+    }
+}
 </script>
 
 <style></style>
 
 <template>
-    <div>
-        <input v-mod type="number" min="1" max="1000" v-model="KString" placeholder="Enter K" />
-
-        <span>K:{{ KString }}</span>
-    </div>
-    <div>
-        
-        <input type="number" min="1" max="1000" v-model="NString" placeholder="Enter N" />
-
-        <span>N:{{ NString }}</span>
-    </div>
-    <button @click="console.log( KString ,  NString)">
+    <button @click="play" >
         active
     </button>
     <div>
